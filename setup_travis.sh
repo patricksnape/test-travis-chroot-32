17e044e79fe8f6b38b7a6ae29354d16c51828469
Patrick Snape <patricksnape@gmail.com>
2015-09-10 10:59:47 +0100

    First attempt at Travis 32 chroot builds

---
+++ setup_travis.sh
@@ -0,0 +1,12 @@
+if [ $T_ARCH -eq 32 ]; then
+  sudo apt-get install debootstrap
+  sudo apt-get install schroot
+  sudo cp precise_1386.conf /etc/schroot/chroot.d/precise_i386.conf
+
+  sudo mkdir -p /srv/chroot/precise_i386
+  debootstrap --variant=buildd --arch i386 precise /srv/chroot/precise_i386 http://archive.ubuntu.com/ubuntu/
+  schroot -l
+  schroot -c precise_i386 -u travis32
+  sudo apt-get install build-essential checkinstall
+  cd ~
+Fi
